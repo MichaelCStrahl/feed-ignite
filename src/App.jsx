722e8bdd873ffcfd1c0f1d7ef3c58b005d03539d
Michael C. Strahl <michaelstrahl.sm@gmail.com>
2023-03-07 08:09:12 -0300

    Dynamic posts

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,36 @@ import styles from './App.module.css'
  * content: String
  */
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/MichaelCStrahl.png',
+      name: 'Michael C. Strahl',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-03-06 08:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-03-07 20:00:00')
+  }
+]
 
 export function App() {
   return (
@@ -21,9 +51,15 @@ export function App() {
       <div className={ styles.wrapper }>
         <Sidebar />
         <main>
-          <Post/>
-
-          <Post/>
+          {posts.map(post => {
+            return (
+              <Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
